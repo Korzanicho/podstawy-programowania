fbac01bccbd3fc8f335f52d5024912b2c437d1b4
Adrian Korzan <adrian.korzan@gmail.com>
2020-06-24 23:50:13 +0200

    (feature): dodanie informacji na stronie głównej

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html lang="pl">
 <?php include_once('./header.php') ?>
+	<p>Możliwe że menu nie będzie działało poprawnie ponieważ wszystkie linki mają postać:</p>
+	<q>href="<?=$_SERVER['HTTP_HOST']?>/Tasks/zad1/view.php"</q>
+	Gdzie początek to zmienna z php - $_SERVER['HTTP_HOST']
+	<p>Aby linki oraz style działały poprawnie, pliki powinny znajdować się w głónym pniu serwera</p>
+	<p>Oczywiście menu oraz style nie były wymagane więc pozwoliłem sobie na taką możliwą niedogodność. Jeśli chodzi o pliki php wszystko powinno działać jak należy</p>
+	<p style="text-align: right;">Adrian Korzan 6900</p>
 </body>
 </html>
